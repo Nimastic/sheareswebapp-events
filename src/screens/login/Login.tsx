8f7a885bdc6f51d20f6d96f379be6f01be14b64f
blaukc <brandonlau1@hotmail.com>
2023-12-25 01:04:24 +0800

    Remove react native paper

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,46 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useContext } from 'react';
-import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { TextInput, Text } from 'react-native-paper';
-import AuthContext from '../../auth/AuthContext';
-import { Button, Theme, XStack, YStack } from 'tamagui';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: 10,
-    },
-    input: {
-        width: "80%"
-    },
-    button: {
-        width: "40%"
-    },
-});
+import { Button, H2, Text, XStack, YStack } from 'tamagui'
 
 interface LoginProps {
-    promptAsync: () => {};
+    promptAsync: () => {}
 }
 
 const Login: React.FC<LoginProps> = ({ promptAsync }) => {
-
     return (
-        <SafeAreaView
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        <YStack
+            fullscreen
+            flex={1}
+            alignItems="center"
+            justifyContent="center"
+            gap="$4"
         >
-            <YStack>
-                <XStack>
-                    <Theme name="light">
-                        <Button size="$6" theme="orange" 
-                        onPress={() => promptAsync()}>Sign In with Google</Button>
-                    </Theme>
-                </XStack>
-            </YStack>
-        </SafeAreaView>
-    );
+            <XStack paddingHorizontal="$5">
+                <H2>Log in</H2>
+            </XStack>
+            <XStack paddingHorizontal="$5">
+                <Text>Welcome back!</Text>
+            </XStack>
+            <XStack alignSelf="center">
+                <Button size="$6" theme="orange" onPress={() => promptAsync()}>
+                    Sign In with Google
+                </Button>
+            </XStack>
+        </YStack>
+    )
 }
 
-export default Login;
+export default Login
